dea132f6f4a3a9076910a890f054366a9aa07b4d
BladeBuru <72576887+BladeBuru@users.noreply.github.com>
2023-11-11 12:22:25 +0100

    fix image and color background switch button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,32 +1,30 @@
-import React, {useContext} from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 import SwitchDarkMode from "../SwitchDakMode/SwitchDarkMode";
-import {DarkModeContext} from "../../context/DarkModeContext";
 
 export default function Navbar() {
-  const {darkMode} = useContext(DarkModeContext);
   return (
     <nav>
       <ul >
-        <li>
-          <Link className={`nav-link `}to="/">
+        <li className={`nav-link `}>
+          <Link to="/">
             Home
           </Link>
         </li>
-        <li>
-          <Link className={`nav-link `} to="/meals">
+        <li className={`nav-link `}>
+          <Link  to="/meals">
             Meals
           </Link>
         </li>
-        <li>
-          <Link className={`nav-link `} to="/cocktails">
+        <li className={`nav-link `}>
+          <Link to="/cocktails">
             Cocktails
           </Link>
         </li>
-        <li>
-          <SwitchDarkMode></SwitchDarkMode>
-        </li>
+        <div className={'switch'}>
+          <SwitchDarkMode ></SwitchDarkMode>
+        </div>
       </ul>
     </nav>
   );
