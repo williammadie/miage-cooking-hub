1457f1ecbd33a79f7af97b3caf5f9b34030a29db
williammadie <william.madie0@gmail.com>
2023-11-15 19:14:55 +0100

    fix switch button storybook

--- src/components/SwitchSetting/SwitchSetting.stories.tsx
+++ src/components/SwitchSetting/SwitchSetting.stories.tsx
@@ -3,22 +3,22 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { StoryFn } from "@storybook/react";
 import SwitchSetting, { SwitchSettingProps } from "./SwitchSetting";
+import { DarkModeProvider } from "../../context/DarkModeContext";
 
 export default {
   component: SwitchSetting,
-  title: "component/SearchBar",
+  title: "component/SwitchSetting",
 };
 
 const Template: StoryFn<SwitchSettingProps> = (args: SwitchSettingProps) => (
-  <BrowserRouter>
-    <SwitchSetting {...args} />
-  </BrowserRouter>
+  <DarkModeProvider>
+    <BrowserRouter>
+      <SwitchSetting {...args} />
+    </BrowserRouter>
+  </DarkModeProvider>
+  
 );
 
 export const Default = Template.bind({});
 
-Default.args = {
-  // receiveMeals: () => {
-  //   console.log("hello world!");
-  // },
-};
+Default.args = {};
